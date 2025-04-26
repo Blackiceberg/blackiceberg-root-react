3ddeb2987a1e4f427aaccc817b80ec23b2738059
Blackiceberg <quenedie.moindze@gmail.com>
2025-04-26 19:38:32 +0200

    Root.render

--- index.jsx
+++ index.jsx
@@ -1,10 +1,22 @@
-import { createElement } from "react";
 import { createRoot } from "react-dom/client";
-const root = createRoot(document.getElementById("root"));
-root.render(
-  <main>
-    <img src="/src/assets/react.svg" alt="react" />
-    <h1>This is another element </h1>
-  </main>
-  )
+const root = createRoot(document.getElementById("root"))
 
+
+function affichage () {
+
+  return(
+      <main>
+        <img src="/src/assets/react.svg" alt="" />
+        <h1>Ngazidja</h1>
+        <ul>
+          <li>good small</li>
+          <li>Natural Flavors</li>
+          <li>Slip Nice, Swim nice</li>
+        </ul>
+      </main>
+  )
+}
+
+root.render(
+  affichage()
+)
