0f9f24b5a15670ce05f6ce4bb83d5eb9344974f3
Blackiceberg <quenedie.moindze@gmail.com>
2025-04-26 14:23:06 +0200

    Root.render

--- index.jsx
+++ index.jsx
@@ -1,7 +1,10 @@
+import { createElement } from "react";
 import { createRoot } from "react-dom/client";
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <main>
+    <img src="/src/assets/react.svg" alt="react" />
+    <h1>This is another element </h1>
+  </main>
+  )
 
-const root = createRoot(document.getElementById("root")).render(
-  <p>
-    Merci <br /> le monde est méchant
-  </p>
-);
